db438e9ec2c0809ccdce4a51044634bc2ef33cc5
Dominic Chen <dominic.chen630@gmail.com>
2024-08-03 00:27:12 -0400

    created frontend structure

---
+++ client/src/app/pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,3 +16,10 @@ module.exports = {
   },
   plugins: [],
 };
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
