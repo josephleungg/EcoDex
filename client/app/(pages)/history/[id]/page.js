69cc1a7b128fdf52d5fd2f3564ac81585ac1bfa8
Joseph Leung <leung.joseph@outlook.com>
2024-08-03 17:05:16 -0400

    Update page.js

--- client/app/(pages)/history/[id]/page.js
+++ client/app/(pages)/history/[id]/page.js
@@ -3,7 +3,17 @@ import React, { useState, useEffect } from 'react';
 
 export default function HistoryItem({ params }) {
   const { id, name } = params;
-  const { itemData, setItemData } = useState({"title":"", "description":"", "typeOfWaste":"", "bioDegradable":"", "decomposeTime":"", "approximateWeight": "", "dimensions": "", "litersOfWaste": "", "image": ""});
+  const [itemData, setItemData] = useState({
+    title: "title",
+    description: "desc",
+    typeOfWaste: "tow",
+    bioDegradable: "bd",
+    decomposeTime: "dt",
+    approximateWeight: "aw",
+    dimensions: "d",
+    litersOfWaste: "low",
+    image: "i"
+  });
 
   // fetching the item data once it is mounted
   useEffect(() => {
@@ -16,9 +26,82 @@ export default function HistoryItem({ params }) {
   //  </Link>
   return (
     <div>
+
+      {/* EcoDex title */}
       <div className="px-2 font-roboto">
         <h1 className="font-black text-5xl tracking-wider my-4">EcoDex</h1>
       </div>
+
+      {/* EcoDex item */}
+      <div className="relative flex flex-col rounded-3xl bg-white shadow-md py-16 px-6 mx-2 mt-16 font-medium text-sm">
+        <div className="relative mb-20">
+          <img
+            src="/images/ecodex.png"
+            alt="leaf"
+            className="absolute top-[-10rem] left-1/2 transform -translate-x-1/2 h-48 w-48 rounded-3xl"
+          />
+        </div>
+
+        <div className="h-0.5 bg-gray-300 mb-3"></div>
+
+        <div className="flex flex-row justify-between">
+          <p className="text-gray-400">Name</p>
+          <p className="">{itemData.title}</p>
+        </div>
+
+        <div className="h-0.5 bg-gray-300 my-3"></div>
+
+        <div className="flex flex-row justify-between">
+          <p className="text-gray-400">Description</p>
+          <p className="">{itemData.description}</p>
+        </div>
+
+        <div className="h-0.5 bg-gray-300 my-3"></div>
+
+        <div className="flex flex-row justify-between">
+          <p className="text-gray-400">Type of Waste</p>
+          <p className="">{itemData.typeOfWaste}</p>
+        </div>
+
+        <div className="h-0.5 bg-gray-300 my-3"></div>
+
+        <div className="flex flex-row justify-between">
+          <p className="text-gray-400">Biodegradable</p>
+          <p className="">{itemData.bioDegradable}</p>
+        </div>
+
+        <div className="h-0.5 bg-gray-300 my-3"></div>
+
+        <div className="flex flex-row justify-between">
+          <p className="text-gray-400">Decompose Time</p>
+          <p className="">{itemData.decomposeTime}</p>
+        </div>
+
+        <div className="h-0.5 bg-gray-300 my-3"></div>
+
+        <div className="flex flex-row justify-between">
+          <p className="text-gray-400">Approximate Weight</p>
+          <p className="">{itemData.approximateWeight}</p>
+        </div>
+
+        <div className="h-0.5 bg-gray-300 my-3"></div>
+
+        <div className="flex flex-row justify-between">
+          <p className="text-gray-400">Dimensions</p>
+          <p className="">{itemData.dimensions}</p>
+        </div>
+
+        <div className="h-0.5 bg-gray-300 my-3"></div>
+
+        <div className="flex flex-row justify-between">
+          <p className="text-gray-400">Liters of Water Used</p>
+          <p className="">{itemData.litersOfWaste}</p>
+        </div>
+
+        <div className="h-0.5 bg-gray-300 mb-3"></div>
+
+      </div>
+
     </div>
   );
 }
