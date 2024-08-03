27a1a403f1de9b996c723566d0e19b960bf9899e
Kush Patel <kush.patel@adevinta.com>
2024-08-03 13:47:25 -0400

    .

--- server/helpers/openAPICalls.py
+++ server/helpers/openAPICalls.py
@@ -6,34 +6,38 @@ load_dotenv("../.env")
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-response = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {
-      "role": "user",
-      "content": [
-        {"type": "text", "text": """Using the image provided, could you give us a response in this format: 
-                                    Title:
-                                    Description:
-                                    Type of Waste: (Garbage/Recycling/Green Bin/Yard Waste/Battery Disposal) 
-                                    Biodegradable: (Yes/No)
-                                    Decompose Time:
-                                    Approximate Weight:
-                                    Approximate Dimensions:
-                                    Amount of Liters of Water to Produce: 
+def openApiCall():
+    
+    response = client.chat.completions.create(
+    
+        model="gpt-4o-mini",
+    
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": """Using the image provided, could you give us a response in this format: 
+                                                Title:
+                                                Description:
+                                                Type of Waste: (Garbage/Recycling/Green Bin/Yard Waste/Battery Disposal) 
+                                                Biodegradable: (Yes/No)
+                                                Decompose Time:
+                                                Approximate Weight:
+                                                Approximate Dimensions:
+                                                Amount of Liters of Water to Produce: 
 
-                                    Try and give exact answers. For anything that youre unsure of, put N/A
-                                    """},
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": "https://www.fixfactory.ca/wp-content/uploads/iPhone-12-and-12-Pro-Battery-Replacement-Fix-Factory-Canada.jpg",
-          },
-        },
-      ],
-    }
-  ],
-  max_tokens=300,
-)
-
-print(response.choices[0])
+                                                Try and give exact answers. For anything that youre unsure of, put N/A
+                                                """},
+                    {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://www.fixfactory.ca/wp-content/uploads/iPhone-12-and-12-Pro-Battery-Replacement-Fix-Factory-Canada.jpg",
+                        },
+                    },
+                ],
+            }
+        ],
+        max_tokens=300,
+    )
+    
+    return response.choices[0]
