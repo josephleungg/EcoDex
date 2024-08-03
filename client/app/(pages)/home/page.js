f24e24984f5130e935722d802d2ff3f287221d8d
Joseph Leung <leung.joseph@outlook.com>
2024-08-03 14:40:11 -0400

    Changes to configs

--- client/app/(pages)/home/page.js
+++ client/app/(pages)/home/page.js
@@ -1,7 +1,22 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+
 export default function Home() {
+    const [headerText, setHeaderText] = useState("Environmental Fact ");
+    const headerSentences = ["recycling is good", "deforestation harms biodiversity", "plastic pollutes oceans", "air pollution kills millions", "climate change raises temperatures","water scarcity affects billions","renewable energy reduces emissions","planting trees combats CO2","overfishing depletes oceans", "sustainable living is the future"];
+
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * headerSentences.length);
+        setHeaderText(headerText + headerSentences[randomIndex]);
+    }, [])
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="">
+
+            <div className="pt-4 flex flex-col justify-center items-center">
+                <h1 className="text-xs text-tertiary-0 font-roboto font-medium">{headerText}</h1>
+            </div>
+            
         </div>
     );
 }
--- client/app/(pages)/layout.js
+++ client/app/(pages)/layout.js
@@ -2,13 +2,23 @@ import Navbar from '../components/navbar';
 import Header from '../components/header';
 
 export default function RootLayout({ children }) {
-    return (
-      <html>
-        <body>
-          <Header />
-            <main className="min-h-screen bg-[#dbdbdb] pt-28 pb-20">{children}</main>
-          <Navbar />
-        </body>
-      </html>
-    );
-  }
+  return (
+    <html>
+      <body>
+        <Header />
+        <main
+          className="min-h-screen bg-[#dbdbdb] py-28"
+          style={{
+            backgroundImage: 'url(/images/ecodex-leaf-opacity.png)',
+            backgroundSize: '80% auto',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+          }}
+        >
+          {children}
+        </main>
+        <Navbar />
+      </body>
+    </html>
+  );
+}
