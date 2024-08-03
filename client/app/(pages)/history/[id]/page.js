a1e04cd46943331d755c66416f939060657771ab
Joseph Leung <leung.joseph@outlook.com>
2024-08-03 16:34:13 -0400

    Update page.js

--- client/app/(pages)/history/[id]/page.js
+++ client/app/(pages)/history/[id]/page.js
@@ -1,7 +1,14 @@
 'use client'; // Ensure this is at the top of the file
+import React, { useState, useEffect } from 'react';
 
 export default function HistoryItem({ params }) {
   const { id, name } = params;
+  const { itemData, setItemData } = useState({"title":"", "description":"", "typeOfWaste":"", "bioDegradable":"", "decomposeTime":"", "approximateWeight": "", "dimensions": "", "litersOfWaste": "", "image": ""});
+
+  // fetching the item data once it is mounted
+  useEffect(() => {
+    console.log('sup')
+  }, []);
 
   // this is how to access this dynamic page
   //  <Link href="/history/1?name=John%20Doe">
@@ -9,9 +16,9 @@ export default function HistoryItem({ params }) {
   //  </Link>
   return (
     <div>
-      <h1>User Profile</h1>
-      <p>User ID: {id}</p>
-      <p>User Name: {name}</p>
+      <div className="px-2 font-roboto">
+        <h1 className="font-black text-5xl tracking-wider my-4">EcoDex</h1>
+      </div>
     </div>
   );
 }
