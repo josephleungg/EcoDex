c293d17965a68ead461da720bd7c2f9fc851b2ea
Kush Patel <kush.patel@adevinta.com>
2024-08-03 23:35:22 -0400

    frontend stuff

--- client/app/(pages)/profile/page.js
+++ client/app/(pages)/profile/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
@@ -9,11 +9,26 @@ export default function Profile() {
     const [userImage, setUserImage] = useState('/images/user.png');
     const [name, setName] = useState('Name');
     const [isEditing, setIsEditing] = useState(false);
+    const [tokens, setTokens] = useState(0);
+    const [contributions, setContributions] = useState(0);
 
     const monthNames = [
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
       ];
+
+    useEffect(() => {
+        try {
+            fetch('http://127.0.0.1:5000/getnumbers')
+                .then((response) => response.json())
+                .then((data) => {
+                    setTokens(data.points);
+                    setContributions(data.contributions);
+                });
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }, [])
 
     const handleImageUpload = (event) => {
         const file = event.target.files[0];
@@ -53,13 +68,13 @@ export default function Profile() {
             </div>
             <h1 className="m-2 font-roboto font-semibold text-tertiary-0 text-lg">Joined {monthNames[new Date().getMonth()]} {new Date().getFullYear()}</h1>
             <div className="flex items-center justify-between">
-                <div className="bg-white w-14 h-10 rounded-lg mr-12 flex items-center justify-around drop-shadow-2xl">
-                    <img src="/images/eco-coin.png" className="h-6 w-6" />
-                    <h1 className="text-tertiary-0 font-bold text-2xl">0</h1>
+                <div className="bg-white p-4 h-10 rounded-lg mr-12 flex items-center justify-around drop-shadow-2xl">
+                    <img src="/images/eco-coin.png" className="h-6 w-6 mr-2" />
+                    <h1 className="text-tertiary-0 font-bold text-2xl">{tokens}</h1>
                 </div>
-                <div className="bg-white w-14 h-10 rounded-lg flex items-center justify-around drop-shadow-2xl">
-                    <img src="/images/bookmark.png" className="h-5 w-5" />
-                    <h1 className="text-tertiary-0 font-bold text-2xl">0</h1>
+                <div className="bg-white p-4 h-10 rounded-lg flex items-center justify-around drop-shadow-2xl">
+                    <img src="/images/bookmark.png" className="h-5 w-5 mr-2" />
+                    <h1 className="text-tertiary-0 font-bold text-2xl">{contributions}</h1>
                 </div>
             </div>
             <div className="mt-4 h-12 w-80 px-8 bg-white drop-shadow-2xl flex items-center justify-between rounded-2xl">
